d6918b4ec90451eb5734908b7dfc36b5ddf91d7f
Emre-11 <144242206+Emre-11@users.noreply.github.com>
2023-10-05 09:14:21 -0400

    Update program.py

--- program.py
+++ program.py
@@ -1,7 +1,8 @@
 # My Python Program
 # Task:  Use the function myFunction to output a simple "Hello World!" statement
 
-def my_Function(Emre):
-def myfunction = "Hello" + Emre
-  return(myfunction)
-Print(myfuntion(Emre))
+def myfunction(Emre):
+    myfunction = "Hello " + Emre
+    return myfunction
+
+print(myfunction("Emre"))
